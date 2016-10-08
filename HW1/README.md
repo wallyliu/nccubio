1a8c71b4a1ae599bcb2fe4d7d585520c09156e63
wallyliu <briskviva@gmail.com>
2016-10-08 13:20:24 +0800

    Add Homework 1

---
+++ HW1/README.md
@@ -0,0 +1,5 @@
+# Propare:
+	put "pam1.txt" at current direction
+# Usage: 
+	R hw1_104753035_wallyliu.R
+
---
+++ HW1/hw1_104753035_wallyliu.R
@@ -0,0 +1,19 @@
+# Yu-Wen Liu's bioInfo Homework1
+# 2016/09/29
+
+# read PAM1 from data
+pam1 <- read.table("/Users/wally/Desktop/NCCU_master/1051/bioinformatric/HW1/pam1.txt")
+
+# construct PAM250 from PAM1
+pam250 <- diag(20)
+for( i in 1:250){
+  pam250 <- pam250 %*% as.matrix(pam1/10000) 
+}
+
+# output PAM250 as a file
+pam250 <- round( as.data.frame(pam250) * 10000, digit = 4)
+pam250 <- rbind( colnames(pam250), pam250)
+colnames(pam250) <- as.factor(colnames(pam1))
+rownames(pam250) <- as.factor(c(" ", rownames(pam1)))
+write.table( pam250, file = "output.txt", sep = "\t", col.names= FALSE)
+
