f4619b2370835403c19d0f8b58eb73defdcf57c7
liuyu-wen <wally@liuyu-wendeMacBook-Air.local>
2016-11-08 22:59:18 +0800

    modify gap open penalty and Readme --2016/11/08

--- HW2/README.md
+++ HW2/README.md
@@ -1,6 +1,6 @@
 # NCCU bioinformatrics HomeWork 2 
 ## Desciption
-Homeword 2 is a project about implementation of __global alignment__ and __local alignment__ of __protein sequence__ with different scoring scheme, and __hw2_104753035_ywliu.R__ is the main program.<br /> User have to provide the some argument listing in the next paragraph<br />
+Homeword 2 is a project about implementation of __global alignment__ and __local alignment__ of __protein sequence__ with affine gap penalty, and __hw2_104753035_ywliu.R__ is the main program.<br /> User have to provide the some argument listing in the next paragraph<br />
 ## Argument:
     --input:  path of input file (e.g. ./test.fasta.txt)
     --score:  path of scoring file (e.g. pam250.txt, pam100.txt)
@@ -10,4 +10,4 @@ Homeword 2 is a project about implementation of __global alignment__ and __local
     --output: name of output file (e.g. ./result.fasta)
     --gap: open/extend
 ## Sxample Script:
-    R hw2_104753035_ywliu.R --input test.fasta.txt --score pam250.txt --aln global --gap_open -10 --gap_extend -2 --output result.fasta --gap open
+    R hw2_104753035_ywliu.R --input test.fasta.txt --score pam250.txt --aln global --gap_open -10 --gap_extend -2 --output result.fasta
--- HW2/hw2_104753035_ywliu.R
+++ HW2/hw2_104753035_ywliu.R
@@ -15,7 +15,6 @@ score_flag <- pmatch( "--score", args)
 aln_flag <- pmatch( "--aln", args)
 gop_flag <- pmatch( "--gap_open", args)
 gep_flag <- pmatch( "--gap_extend", args)
-gap_flag <- pmatch( "--gap", args)
 
 input<-c(args[(input_flag+1)])
 output<-c(args[output_flag+1])
@@ -23,7 +22,6 @@ score<-c(args[score_flag+1])
 aln<-c(args[aln_flag+1])
 gop<-as.numeric(args[gop_flag+1])
 gep<-as.numeric(args[gep_flag+1])
-gap<-c(args[gap_flag+1])
 
 ####################################
 #       READ DATA FROM FILE        #
@@ -50,17 +48,20 @@ scoreMatrix<-as.matrix(scoreMatrix)
 ####################################
 alignmentFunc <- function(sequence, seq_length, aln_length, scoreMatrix, gop, gap, aln)
 {
-    # Constuct ALLIGNMENT SCORE TABLE
+    # Constuct ALLIGNMENT SCORE TABLE (globalTable)
     seqTitle1 <- c( c(strsplit(sequence[1], "")[[1]]) )
     seqTitle2 <- c( c(strsplit(sequence[2], "")[[1]]) )
     globalTable <- matrix(data=0, nrow=seq_length[1]+1, ncol=seq_length[2]+1, dimname=list(c("-", seqTitle1), c("-", seqTitle2)))
 
-    # Initialize globalTable which is be used to store ALIGNMENT SCORE.
+    # Initialize globalTable used to store ALIGNMENT SCORE.
     if( aln == "global")
     {
         tmp_gep <- ifelse(gep<0, -gep, gep)
-        t_v1 <- seq(0, seq_length[2]*tmp_gep, tmp_gep)
-        t_v2 <- seq(0, seq_length[1]*tmp_gep, tmp_gep)
+        tmp_gop <- ifelse(gep<0, -gop, gop)
+
+        t_v1 <- seq( tmp_gop-tmp_gep, tmp_gop+(seq_length[2]-1)*tmp_gep, tmp_gep)
+        t_v2 <- seq( tmp_gop-tmp_gep, tmp_gop+(seq_length[1]-1)*tmp_gep, tmp_gep)
+        
         if( gep<0 ){
             globalTable[1,] <- -t_v1
             globalTable[,1] <- -t_v2
@@ -68,6 +69,18 @@ alignmentFunc <- function(sequence, seq_length, aln_length, scoreMatrix, gop, ga
             globalTable[1,] <- t_v1
             globalTable[,1] <- t_v2
         }
+        globalTable[1,1] <- 0
+    }
+    # Initialize tracebackMatrix used to stroe TRACKBACK INFORMATION
+    if( aln == "global")
+    {
+        tracebackMatrix[1,] <<- "insertion"
+        tracebackMatrix[,1] <<- "deletion"
+        tracebackMatrix[1,1] <<- "substitution"
+    }else
+    {
+        tracebackMatrix[1,] <<- "no"
+        tracebackMatrix[,1] <<- "no"
     }
 
     # Calculate score word by word
@@ -75,42 +88,51 @@ alignmentFunc <- function(sequence, seq_length, aln_length, scoreMatrix, gop, ga
     {
         for(j in 1:length(seqTitle2))
         {
+            # decide insertion penalty
+            insertion_penalty <- gop
+            if( tracebackMatrix[i+1, j] == "substitution")
+            {
+                insertion_penalty <- gop
+            }else if( tracebackMatrix[i+1, j] == "insertion" || tracebackMatrix[i+1, j] == "deletion" )
+            {
+                insertion_penalty <- gep
+            }
+            
+            # decide deletion penalty
+            deletion_penalty <- gop
+            if( tracebackMatrix[i, j+1] == "substitution")
+            {
+                deletion_penalty <- gop
+            }else if( tracebackMatrix[i, j+1] == "insertion" || tracebackMatrix[i, j+1] == "deletion" )
+            {
+                deletion_penalty <- gep
+            }
+
+            # get comparison character
             a<-substring(sequence[1], i, i)
             b<-substring(sequence[2], j, j)
  
-            # check penalty according to different socring scheme
-            openPenalty <- 0
-            if( gap =="open"){
-                if( i==1 && j==1 )
-                {
-                    openPenalty <- gop
-                }else if( !is.na( which( c( tracebackMatrix[i-1][j-1], tracebackMatrix[i][j-1], tracebackMatrix[i-1][j]) =="substitution")[1] ))
-                {
-                    openPenalty <- gop
-                }
-            }
-
             # calculate socre of each column
             if((a != "-")&&(b != "-"))
             {
                 # Substitution, Insertion, Deletion
                 if( aln == "global")
                 {   # global alignment
-                    value <- c( globalTable[i,j]+scoreMatrix[a,b], globalTable[i+1,j]+gep+openPenalty, globalTable[i,j+1]+gep+openPenalty)
+                    value <- c( globalTable[i,j]+scoreMatrix[a,b], globalTable[i+1,j]+insertion_penalty, globalTable[i,j+1]+deletion_penalty)
                     globalTable[i+1,j+1] <- max(value)
                 }else
                 {   # local alignment
-                    value <- c( globalTable[i,j]+scoreMatrix[a,b], globalTable[i+1,j]+gep+openPenalty, globalTable[i,j+1]+gep+openPenalty, 0)
+                    value <- c( globalTable[i,j]+scoreMatrix[a,b], globalTable[i+1,j]+insertion_penalty, globalTable[i,j+1]+deletion_penalty, 0)
                     globalTable[i+1,j+1] <- max(value)
                 }
             }else{
                 if( aln == "global")
                 {   # global alignment
-                    value <- max( globalTable[i+1,j]+gep+openPenalty, globalTable[i,j+1]+gep+openPenalty)
+                    value <- max( globalTable[i+1,j]+insertion_penalty, globalTable[i,j+1]+deletion_penalty)
                     globalTable[i+1,j+1] <- max(value)
                 }else
                 {   # local alignment
-                    value <- max( globalTable[i+1,j]+gep+openPenalty, globalTable[i,j+1]+gep+openPenalty, 0)
+                    value <- max( globalTable[i+1,j]+insertion_penalty, globalTable[i,j+1]+deletion_penalty, 0)
                     globalTable[i+1,j+1] <- max(value)
                 }
             }
@@ -118,11 +140,13 @@ alignmentFunc <- function(sequence, seq_length, aln_length, scoreMatrix, gop, ga
             # Constuct TRACEBACKMATRIX used to traceback sequence
             direction <- which( value==max(value))[1]
             if( direction == 1 ){
-                tracebackMatrix[i,j] <<- "substitution"
+                tracebackMatrix[i+1,j+1] <<- "substitution"
             }else if( direction == 2){
-                tracebackMatrix[i,j] <<- "insertion"
+                tracebackMatrix[i+1,j+1] <<- "insertion"
+            }else if( direction == 3){
+                tracebackMatrix[i+1,j+1] <<- "deletion"
             }else{
-                tracebackMatrix[i,j] <<- "deletion"
+                tracebackMatrix[i+1,j+1] <<- "no"
             }
         }
     }
@@ -145,30 +169,36 @@ tracebackString <- function( finalMatrix, sequence, aln)
     }else
     {   # local alignment
         index <- which( finalMatrix == max(finalMatrix), arr.ind = TRUE)
-        i <- index[1]
-        j <- index[2]    
+        i <- index[nrow(index),][1]
+        j <- index[nrow(index),][2]    
     }
+
     # Substitution, Insertion, Deletion
     while( i>1 && j>1 )
     {
-        if( tracebackMatrix[i-1,j-1] == "substitution")
+        if( tracebackMatrix[i,j] == "substitution")
         {   # Substitution
             finalString1 <- paste( substring(sequence[1], i-1, i-1), finalString1)
             finalString2 <- paste( substring(sequence[2], j-1, j-1), finalString2)
             i <- i-1
             j <- j-1
-        }else if( tracebackMatrix[i-1,j-1] == "insertion")
+        }else if( tracebackMatrix[i,j] == "insertion")
         {   # Insertion
             finalString1 <- paste( "-", finalString1)
             finalString2 <- paste( substring(sequence[2], j-1, j-1), finalString2)
             i <- i
             j <- j-1
-        }else
+        }else if( tracebackMatrix[i,j] == "deletion")
         {   # Deletion
             finalString1 <- paste( substring(sequence[1], i-1, i-1), finalString1)
             finalString2 <- paste( "-", finalString2)
             i <- i-1
             j <- j
+        }else
+        {   # Do Nothing -> local alignment
+            finalString1 <- paste( substring(sequence[1], i-1, i-1), finalString1)
+            finalString2 <- paste( substring(sequence[2], j-1, j-1), finalString2)
+            return(c(finalString1, finalString2))
         }
     }
     return(c(finalString1, finalString2))
@@ -177,7 +207,7 @@ tracebackString <- function( finalMatrix, sequence, aln)
 ####################################
 #          MAIN FUNCTION           #
 ####################################
-tracebackMatrix <<- matrix(data="", nrow=nchar(sequence[1]), ncol=nchar(sequence[2]))
+tracebackMatrix <<- matrix(data="", nrow=nchar(sequence[1])+1, ncol=nchar(sequence[2])+1)
 finalMatrix <- alignmentFunc(sequence, seq_length, aln_length, scoreMatrix, gop, gap, aln)
 final <- tracebackString(finalMatrix, sequence, aln)
 finalData <- c( paste(">",seq_name[1]), final[1], paste(">",seq_name[2]), final[2])
